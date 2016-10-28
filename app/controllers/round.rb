a05e0d8b65a683925171086a33225a8f251e2694
Scott Dixon <email": null>
2016-10-28 16:23:59 -0400

    Fix routing in rounds controller

--- app/controllers/round.rb
+++ app/controllers/round.rb
@@ -1,5 +1,6 @@
 post '/rounds' do
+  puts params
   @deck = Deck.find(params[:deck_id])
   @round = Round.create(user: session_user, deck: @deck)
-  redirect '/decks/<%= @deck.id %>/cards/1'
+  redirect "/decks/#{params[:deck_id]}/cards/1"
 end
