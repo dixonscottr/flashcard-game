4850053b5017127899f111de4ee7174cd2264846
Scott Dixon <email": null>
2016-10-28 16:00:27 -0400

    Fix interpolation in round controller

--- app/controllers/round.rb
+++ app/controllers/round.rb
@@ -1,5 +1,5 @@
 post '/rounds' do
   @deck = Deck.find(params[:deck_id])
   @round = Round.create(user: session_user, deck: @deck)
-  redirect '/decks/#{@deck.id}/cards/1'
+  redirect '/decks/<%= @deck.id %>/cards/1'
 end
